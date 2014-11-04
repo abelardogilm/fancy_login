dab2ea1b1db0a7e1ebecc5dc66ec8e8dc9f2ac5e
Luka <luigi7up@gmail.com>
2014-11-04 11:52:17 +0100

    freed coffee-rails and sass-rails versions block that was unabling update in PF due to the rails 4 there

--- lib/fancy_login/version.rb
+++ lib/fancy_login/version.rb
@@ -1,3 +1,3 @@
 module FancyLogin
-  VERSION = "1.0.25" # Fixed problem where desktop computers with low height were considered mobiles
+  VERSION = "1.0.26" # Unlocked coffee-rails and sass-rails versions
 end
